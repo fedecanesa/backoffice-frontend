ba3c7f5f54e814f488eec6af34b64bad872ec176
Matias Aquino <m4tias.a@gmail.com>
2020-11-18 12:07:30 -0300

    backoffice build added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
 import CardList from "./Components/CardList/CardList";
+import SearchBox from "./Components/SearchBox/SearchBox";
+import ButtonAdd from "./Components/ButtonAdd/ButtonAdd";
 
 function App() {
     return (
         <div className="App">
             <Router>
                 <NavBar />
+                <h2 className="app-subtitle">Backoffice ServiOficios</h2>
+
+                <div className="main-actions">
+                    <SearchBox />
+                    <ButtonAdd />
+                </div>
                 <CardList />
             </Router>
         </div>
