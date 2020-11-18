dcad2d52e55871fabdf1c83e3c6229811d24d465
Matias Aquino <m4tias.a@gmail.com>
2020-11-18 12:05:01 -0300

    ButtonAdd added

---
+++ src/Components/ButtonAdd/ButtonAdd.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ButtonAdd extends Component {
+    render() {
+        return (
+            <button className="buttonadd">
+                <i class="fas fa-plus"></i>
+                Agregar
+            </button>
+        );
+    }
+}
+
+export default ButtonAdd;
