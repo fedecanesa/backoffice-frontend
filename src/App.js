7d3bee3693a7c9446fb161b7e31d0cd64f80e9ec
Nahuel <nahuel.trucco.bkn@gmail.com>
2020-11-21 23:35:10 -0300

    The component and routes structure has been reorganized.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
-import ABMPage from "./Components/ABMPage/ABMPage";
+import ProfessionalsPage from "./Components/ProfessionalsPage/ProfessionalsPage";
+import PendingsPage from "./Components/PendigsPage/PendingsPage";
 import Login from "./Components/Login/Login";
-import CardList from "./Components/CardList/CardList";
+import CardList from "./Components/ProfessionalsList/ProfessionalsList";
 import React from "react";
 import SearchBox from "./Components/SearchBox/SearchBox";
-import ButtonAdd from "./Components/ButtonAdd/ButtonAdd";
 
 
 export default class App extends React.Component {
@@ -15,29 +15,10 @@ export default class App extends React.Component {
         super();
 
         this.state = {
-            pending: [],
-            banners: [],
-            arrayWorkerToShow:[]
+            
+
         }
     }
-
-
-    componentDidMount(){
-        fetch("https://api-servi-oficios.herokuapp.com/pendings") /* TODO */
-        .then((response)=>response.json())
-        .then((jsonResponse)=>{this.setState({pending: jsonResponse})})
-        .catch((error)=>{ /* TODO catch handler */ });
-
-        fetch("https://api-servi-oficios.herokuapp.com/pendings") /* TODO */
-        .then((response)=>response.json())
-        .then((jsonResponse)=>{this.setState({pending: jsonResponse})})
-        .catch((error)=>{ /* TODO catch handler */ });
-    }
-
-    search = (array)=>{
-        this.setState({ arrayWorkerToShow: array });
-    }
-
 
     render() {
         const { pending , banners, arrayWorkerToShow } = this.state;
@@ -51,26 +32,23 @@ export default class App extends React.Component {
                         <Login/>
                     </Route>
 
-                    <Route exact path="/backoffice">
+                    <Route exact path="/profesionales">
                         <NavBar logged={true}/> 
                         <h2 className="app-subtitle">Backoffice ServiOficios</h2>
-                        <ABMPage />
+                        <h4 className="app-description ">Lista de profesionales</h4>
+                        <ProfessionalsPage />
                     </Route>
 
                     <Route exact path="/pendientes">
                         <NavBar logged={true}/>
-                        <div className="main-actions">
-                            <SearchBox />   
-                        </div>
-                       <CardList arrayWorkerToShow={arrayWorkerToShow.length > 0 ? arrayWorkerToShow : pending} />
+                        <h2 className="app-subtitle">Backoffice ServiOficios</h2>
+                        <h4 className="app-description ">Pendientes de Alta</h4>
+                        <PendingsPage />
                     </Route>
 
                     <Route exact path="/banners">
                         <NavBar logged={true}/>
-                        <div className="main-actions">
-                            <SearchBox search={this.search} />             
-                        </div>
-                       <CardList arrayWorkerToShow={this.state.banners} />
+                        {/* TODO BannersPage */}
                     </Route>
 
                 </Switch>
