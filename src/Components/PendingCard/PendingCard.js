e90a821415809d017e641151f5722b9e044d3fac
Nahuel <nahuel.trucco.bkn@gmail.com>
2020-11-21 23:41:45 -0300

    CardList have been divided into PendingList and ProfessionalList

---
+++ src/Components/PendingCard/PendingCard.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+export default class PendingCard extends Component {
+
+    seeDetailsHandler = () => {
+        
+    }
+
+    render() {
+
+        const { registrationDate, name, job, zone } = this.props;
+
+        return (
+            <>
+                <tr className="card-row">
+                    <td className="card-date"></td>
+                    <td>
+                    {
+                        name[0].toUpperCase()+name.slice(1, name.indexOf(" "))
+                        + " " 
+                        + name[name.indexOf(" ")+1].toUpperCase()+name.slice(name.indexOf(" ")+2)
+                    }
+                    </td>
+                    <td>{ job[0].toUpperCase()+job.slice(1) }</td>
+                    <td>{ zone[0].toUpperCase()+zone.slice(1) }</td>
+                    <td className="buttons-container">
+                        <button 
+                            className="card-button button-see"
+                            onClick={this.seeDetailsHandler}
+                        >
+                            <i className="fas fa-eye" />
+                            {" Ver ficha"}
+                        </button>                    
+                    </td>
+                </tr>
+            </>
+        );
+    }
+}
+
---
+++ src/Components/ProfessionalCard/ProfessionalCard.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+
+class Card extends Component {
+    handleClickDelete = (e) => {
+        this.props.handleClickDelete(e.target.value);
+    }
+
+    render() {
+
+        const { _id, name, job, zone } = this.props;
+
+        return (
+            <>
+                <tr className="card-row">
+                    <td className="card-id">
+                        <CopyToClipboard text={_id}> 
+                            <button className="copy-id" onClick={this.handleClick}>Copiar ID</button>
+                        </CopyToClipboard>
+                    </td>
+                    <td>
+                    {
+                        name && (
+                            name[0].toUpperCase()+name.slice(1, name.indexOf(" "))
+                            + " " 
+                            + name[name.indexOf(" ")+1].toUpperCase()+name.slice(name.indexOf(" ")+2)
+                        )
+                    }
+                    </td>
+                    <td>{ job && ( job[0].toUpperCase()+job.slice(1) ) }</td>
+                    <td>{ zone && ( zone[0].toUpperCase()+zone.slice(1) ) }</td>
+                    <td className="buttons-container">
+                        <button className="card-button button-edit">
+                            <i className="fas fa-edit"></i>
+                            Editar
+                        </button>
+                        <button title="¿Esta seguro que desea eliminarlo?" className="card-button button-delete" value={this.props._id} onClick={this.handleClickDelete}>
+                            <i className="fas fa-trash-alt"></i>
+                            Borrar
+                        </button>
+                    </td>
+                </tr>
+            </>
+        );
+    }
+}
+
+export default Card;
